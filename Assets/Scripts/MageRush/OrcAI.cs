e86dc5b00ceff5e6b85a42892908c48e5412647a
dvrisa <38985578+Riku-S@users.noreply.github.com>
2024-01-27 17:16:04 +0200

    Make orc follow the player

---
+++ Assets/Scripts/MageRush/OrcAI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class OrcAI : MonoBehaviour
+{
+    public float speed = 5;
+    public float seeDistance = 20;
+    public CharacterController characterController;
+
+    float seeDistanceSquared;
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeDistanceSquared = seeDistance * seeDistance; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerInput[] players = FindObjectsByType<PlayerInput>(FindObjectsSortMode.None);
+        if (players.Length > 0)
+        {
+            Transform closest = players[0].transform;
+            for (int i = 1; i < players.Length; i++)
+            {
+                Transform candidate = players[i].transform;
+                if ((transform.position - candidate.position).sqrMagnitude < (transform.position - closest.position).sqrMagnitude)
+                {
+                    closest = candidate;
+                }
+            }
+
+            if ((transform.position - closest.position).sqrMagnitude < seeDistanceSquared)
+            {
+                Debug.Log("Move here");
+                characterController.MoveToDirection(closest.position - transform.position);
+            }
+        }
+    }
+
+
+}
